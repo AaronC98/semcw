c79c55b5c3e01831dd66b3a35e181bc9727512d7
Aaron Campbell <aaron.jcam98@gmail.com>
2019-02-17 00:16:50 +0000

    Adding City and Country classes.

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,15 @@
+package com.napier.sem;
+
+
+public class City
+{
+    public int ID;
+
+    public String name;
+
+    public String CountryCode;
+
+    public String district;
+
+    public int population;
+}
