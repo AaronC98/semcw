76ddf6c8961a35495246fd502683794595759cc2
Aaron Campbell <aaron.jcam98@gmail.com>
2019-02-25 13:16:53 +0000

    Adding an SQL statement to print all countries in the world in order of descending population.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,7 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
@@ -9,6 +10,11 @@ public class App {
 
         // Connect to database
         a.connect();
+
+        //All countries in the world organised in descending order
+        System.out.println("All Countries in the World: ");
+        ArrayList<Country> countries = a.populationWorldDesc();
+        a.displayCountry(countries);
 
         // Disconnect from database
         a.disconnect();
@@ -61,6 +67,46 @@ public class App {
             } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public ArrayList<Country> populationWorldDesc() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Name, Population "
+                            + "FROM country "
+                            + "ORDER BY Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (rset.next()) {
+
+                Country country = new Country();
+                country.Name = rset.getString("Name");
+                country.Population = rset.getInt("Population");
+                countries.add(country);
+            }
+            return countries;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get details");
+            return null;
+        }
+    }
+
+    public void displayCountry(ArrayList<Country> countries) {
+        // Print header
+        System.out.println(String.format("%-10s %-20s", "Name", "Population"));
+        // Loop over all employees in the list
+        for (Country country : countries) {
+            String ctry_string =
+                    String.format("%-10s %-20s",
+                            country.Name, country.Population);
+            System.out.println(ctry_string);
         }
     }
 }
